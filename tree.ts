7668a71901eb2ed87ea7d0392b1a33389d6c17f6
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-21 10:07:30 +0200

    Update tree.ts

--- tree.ts
+++ tree.ts
@@ -6,71 +6,73 @@ const pipes = [
     '   '
 ]
 
-let depth = 0;
-let paths : string[] = ['./folder/']
+let depth : boolean[] = [];
+let paths : string[] = ['../ESD-Scanner/']
 
 console.log(pipes[0]);
 
-function readDir(entryPath : string[], depth : number, inLastFolder = false, lastInFolder = false) {
+function generateString(
+    name : string,
+    depths : boolean[],
+    isFile : boolean,
+    isDirectory : boolean,
+    isSymlink : boolean,
+    isLast : boolean,
+) : string {
+    let indentString = ''
+    depths.forEach((depth) => {
+        if (depth) {
+            indentString += '│  ';
+        } else {
+            indentString += '   ';
+        }
+    })
+
+    if (isLast) {
+        indentString += '└─ ';
+    } else {
+        indentString += '├─ ';
+    }
+
+    if (isFile) {
+        indentString += `"${name}"`;
+    }
+
+    if (isDirectory) {
+        indentString += `[${name}]`;
+    }
+
+    if (isSymlink) {
+        indentString += `<${name}>`;
+    }
+
+    return indentString
+}
+
+function readDir(entryPath : string[], depths : boolean[] = []) {
     const dirEntries : Deno.DirEntry[] = [];
+    // console.log('Reading dir:', entryPath.join(''));
+    
     for (const dirEntry of Deno.readDirSync(entryPath.join(''))) {
         dirEntries.push(dirEntry);
     }
     dirEntries.forEach((dir, dirIndex, dirs) => {
-        
-        if (dir.isFile) {
-            if (dirIndex < dirs.length - 1) {
-                lastInFolder = false;
-                if (inLastFolder) {
-                    console.log(`${pipes[1]}${pipes[4].repeat(depth - 1)}${pipes[2]}${dir.name}`, inLastFolder, lastInFolder);
-                } else {
-                    console.log(`${pipes[1].repeat(depth)}${pipes[2]}${dir.name}`, inLastFolder, lastInFolder);
-                }
-            } else {
-                lastInFolder = true;
-                if (inLastFolder) {
-                    console.log(`${pipes[1]}${pipes[4].repeat(depth - 1)}${pipes[3]}${dir.name}`, inLastFolder, lastInFolder);
-                } else {
-                    console.log(`${pipes[1].repeat(depth)}${pipes[3]}${dir.name}`, inLastFolder, lastInFolder);
-                }
-            }
-        } else
+        const isLast = (dirIndex == dirs.length - 1);
+
+        console.log(generateString(dir.name, depths, dir.isFile, dir.isDirectory, dir.isSymlink, isLast));
 
         if (dir.isDirectory) {
-            entryPath.push(dir.name + '/');
-
-            if (dirIndex < dirs.length - 1) {
-                lastInFolder = false;
-                if (inLastFolder) {
-                    console.log(`${pipes[1]}${pipes[4].repeat(depth - 1)}${pipes[2]}[${dir.name}]`, inLastFolder, lastInFolder);
-                } else {
-                    console.log(`${pipes[4].repeat(depth)}${pipes[2]}[${dir.name}]`, inLastFolder, lastInFolder);
-                }
-                inLastFolder = false;
-            } else {
-                lastInFolder = true;
-                if (inLastFolder) {
-                    console.log(`${pipes[1]}${pipes[4].repeat(depth - 1)}${pipes[3]}[${dir.name}]`, inLastFolder, lastInFolder);
-                } else {
-                    console.log(`${pipes[1].repeat(depth)}${pipes[3]}[${dir.name}]`, inLastFolder, lastInFolder);
-                }
-                inLastFolder = true;
-            }
-            
-            readDir(entryPath, depth + 1, inLastFolder);
-
-            entryPath.pop();
+            depths.push(!isLast);
+            entryPath.push(`${dir.name}/`);
+            readDir(entryPath, depths);
         }
+
+        
+        
     })
+
+    depths.pop();
+    entryPath.pop();
 }
 
 readDir(paths, depth);
-
-function generateFileString(
-    name : string,
-    depth : number,
-    isLastFile : boolean,
-    isParentLastFolder : boolean
-) {
-    
-}
