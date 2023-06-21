dc4372397ec4ca3ce8f648126425b389ab912436
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-21 14:11:13 +0200

    Update tree.ts

--- tree.ts
+++ tree.ts
@@ -1,5 +1,12 @@
+import { TreeOptions } from "./interface/tree_options.d.ts";
 import { readDir } from "./read_dir.ts";
 
-export function tree(rootDirectory : string, color = true) {
-    return readDir([rootDirectory], color)
+export function tree(rootPath = '.', treeOptions? : TreeOptions) {
+    const dir = readDir([rootPath], [])
+    return {
+        treeString: dir.treeString,
+        files: dir.dirProperties.files,
+        symlinks: dir.dirProperties.symlinks,
+        directories: dir.dirProperties.directories,
+    }       
 }
